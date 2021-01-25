1d9a548608ec837a9602038ca2c2b8d86b76e514
Gary Kono <gary.a.kono@gmail.com>
2021-01-24 22:31:21 -0800

    Added animations for soldier and infected_unit, and updated main.js to run their scripts. Just changed index.html to run the entities.

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var ASSET_MANAGER = new AssetManager();
 
 ASSET_MANAGER.queueDownload("./sprites/tile.png");
 ASSET_MANAGER.queueDownload("./sprites/black.png");
+ASSET_MANAGER.queueDownload("./sprites/soldier.png");
+ASSET_MANAGER.queueDownload("./sprites/infected_unit.png");
 
 ASSET_MANAGER.downloadAll(function () {
 	var canvas = document.getElementById('gameWorld');
@@ -13,6 +15,8 @@ ASSET_MANAGER.downloadAll(function () {
 	PARAMS.CANVAS_HEIGHT = canvas.height;
 
 	gameEngine.init(ctx);
-	gameEngine.addEntity(new Map(gameEngine));
+	//gameEngine.addEntity(new Map(gameEngine));
+	//gameEngine.addEntity(new Soldier(gameEngine, 1, 1))
+	gameEngine.addEntity(new InfectedUnit(gameEngine, 1, 1))
 	gameEngine.start();
 });
