5e796be147348bbdcee33841bb3ff990877b0ceb
Gary Kono <gary.a.kono@gmail.com>
2021-02-04 15:59:23 -0800

    Added zombie spawner, added mouse click and drop building functions to buildings 'StoneGateHorizontal' to 'WoodWall' (alphabetical)

--- buildings/WoodGateHorizontal.js
+++ buildings/WoodGateHorizontal.js
@@ -1,6 +1,6 @@
 class WoodGateHorizontal { 
-   constructor(game, x, y) {
-       Object.assign(this, { game, x, y });
+   constructor(game) {
+       Object.assign(this, { game });
        this.health = 100;
 
        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/buildings.png");
@@ -21,6 +21,14 @@ class WoodGateHorizontal {
       const xOffset = 20;
       const yOffset = 20;
 
-      ctx.drawImage(this.spritesheet, startX, startY, width, height, this.x, this.y, width*2, height*2); 
+      if (this.game.mouse && this.followMouse) {
+         var mouse = this.game.mouse;
+         ctx.drawImage(this.spritesheet, mouse.x * PARAMS.BLOCKWIDTH, mouse.y * PARAMS.BLOCKWIDTH, 64, 64);
+     }
+
+     if(!this.followMouse){
+         console.log(this.x - (this.game.camera.cameraX * PARAMS.BLOCKWIDTH));
+         ctx.drawImage(this.spritesheet, (this.x - this.game.camera.cameraX) * PARAMS.BLOCKWIDTH, (this.y - this.game.camera.cameraY) * PARAMS.BLOCKWIDTH, width, height);
+     }
    };
 };
--- enemySpawner.js
+++ enemySpawner.js
@@ -9,19 +9,28 @@ class EnemySpawner {
         this.spawnThirdWave = false;
         this.spawnFourthWave = false;
 
-        this.path1 = { startX : 2496 / 64, startY : -64, path : [
-            { x: 2496 / 64, y: 480 / 64 }, 
-            { x: 2112 / 64, y: 480 / 64 }, 
-            { x: 2112 / 64, y: 1050 / 64 },
-            { x: 2304 / 64, y: 1050 / 64 }, 
-            { x: 2304 / 64, y: 1420 / 64 }, 
-            { x: 1500 / 64, y: 1420 / 64 }
-            ]};
+        this.path1 = { 
+            startX : 2496 / 64, startY : -1, path : [
+            { x: 2496 / 64, y: 448 / 64 }, 
+            { x: 2112 / 64, y: 448 / 64 }, 
+            { x: 2112 / 64, y: 1024 / 64 },
+            { x: 2304 / 64, y: 1024 / 64 }, 
+            { x: 2304 / 64, y: 1408 / 64 }, 
+            { x: 1152 / 64, y: 1408 / 64 },
+            { x: 1152 / 64, y: 2240 / 64 },
+            { x: 1792 / 64, y: 2240 / 64 }
+            ]
+        };
 
-        this.path2 = { startX : 25, startY : 51, path : [
-            { x: 25, y: 32 }
+        this.path2 = { startX : 23, startY : 51, path : [
+            { x: 22, y: 47 },
+            { x: 22, y: 42 },
+            { x: 28, y: 42 },
+            { x: 28, y: 35 }
             ]};
     }
+
+    
 
     //Nothing needs to be drawn for the spawner
     draw() {
@@ -35,15 +44,12 @@ class EnemySpawner {
             case this.timeElapsed > 0.5 && !this.spawnFirstWave:
                 //Path 1
                 for (var i = 0; i < 2; i++) {
-                    console.log(this.path1.startX);
-                    console.log(this.path1.startY + (i * -20));
-                    console.log(this.path1.path);
-                    this.game.addEntity(new InfectedUnit(this.game, this.path1.startX, this.path1.startY + (i * -20), this.path1.path));
+                    this.game.addEntity(new InfectedUnit(this.game, this.path1.startX, this.path1.startY + (i * -1), this.copyPath(this.copyPath(this.path1.path))));
                 }
 
                 //Path 2
                 for (var i = 0; i < 3; i++) {
-                    this.game.addEntity(new InfectedUnit(this.game, this.path2.startX, this.path2.startY + (i * 32), this.path2.path));
+                    this.game.addEntity(new InfectedUnit(this.game, this.path2.startX, this.path2.startY + (i * 1), this.copyPath(this.copyPath(this.path2.path))));
                 }
                 this.spawnFirstWave = true;
                 break;
@@ -52,16 +58,16 @@ class EnemySpawner {
             case this.timeElapsed > 10 && !this.spawnSecondWave:
                 //Path 1
                 for (var i = 0; i < 4; i++) {
-                    this.game.addEntity(new InfectedUnit(this.game, this.path1.startX, this.path1.startY + (i * -32), this.path1.path));
+                    this.game.addEntity(new InfectedUnit(this.game, this.path1.startX, this.path1.startY + (i * -1), this.copyPath(this.path1.path)));
                 }
-                this.game.addEntity(new InfectedVenom(this.game, this.path1.startX, this.path1.startY + (4 * -32), this.path1.path));
+                this.game.addEntity(new InfectedVenom(this.game, this.path1.startX, this.path1.startY - 4, this.copyPath(this.path1.path)));
 
                 //Path 2
                 for (var i = 0; i < 4; i++) {
-                    this.game.addEntity(new InfectedUnit(this.game, this.path2.startX, this.path2.startY + (i * 32), this.path2.path));
+                    this.game.addEntity(new InfectedUnit(this.game, this.path2.startX, this.path2.startY + (i * 1), this.copyPath(this.path2.path)));
                 }
-                this.game.addEntity(new InfectedVenom(this.game, this.path2.startX, this.path2.startY + (2.5 * -32), this.path2.path));
-                this.game.addEntity(new InfectedVenom(this.game, this.path2.startX, this.path2.startY + (4.5 * -32), this.path2.path));
+                this.game.addEntity(new InfectedVenom(this.game, this.path2.startX, this.path2.startY + 4, this.copyPath(this.path2.path)));
+                this.game.addEntity(new InfectedVenom(this.game, this.path2.startX, this.path2.startY + 5, this.copyPath(this.path2.path)));
                 this.spawnSecondWave = true;
                 break;
 
@@ -69,24 +75,24 @@ class EnemySpawner {
             case this.timeElapsed > 20 && !this.spawnThirdWave:
                 //Path 1
                 for (var i = 0; i < 7; i++) {
-                    this.game.addEntity(new InfectedUnit(this.game, this.path1.startX, this.path1.startY + (i * -32), this.path1.path));
+                    this.game.addEntity(new InfectedUnit(this.game, this.path1.startX, this.path1.startY + (i * -1), this.copyPath(this.path1.path)));
                 }
                 for (var i = 0; i < 4; i++) {
-                    this.game.addEntity(new InfectedVenom(this.game, this.path1.startX, this.path1.startY + (i * -32 * 0.5), this.path1.path));
+                    this.game.addEntity(new InfectedVenom(this.game, this.path1.startX, this.path1.startY + (i * -2 - 3), this.copyPath(this.path1.path)));
                 }
                 for (var i = 0; i < 5; i++) {
-                    this.game.addEntity(new InfectedHarpy(this.game, this.path1.startX, this.path1.startY + (i * -32 - 10), this.path1.path));
+                    this.game.addEntity(new InfectedHarpy(this.game, this.path1.startX, this.path1.startY + (i * -1.5 - 0.2), this.copyPath(this.path1.path)));
                 }
 
                 //Path 2
                 for (var i = 0; i < 9; i++) {
-                    this.game.addEntity(new InfectedUnit(this.game, this.path2.startX, this.path2.startY + (i * 32), this.path2.path));
+                    this.game.addEntity(new InfectedUnit(this.game, this.path2.startX, this.path2.startY + (i * 1), this.copyPath(this.path2.path)));
                 }
                 for (var i = 0; i < 5; i++) {
-                    this.game.addEntity(new InfectedUnit(this.game, this.path2.startX, this.path2.startY + (i * 32 * 0.5), this.path2.path));
+                    this.game.addEntity(new InfectedVenom(this.game, this.path2.startX, this.path2.startY + (i * 2 + 1), this.copyPath(this.path2.path)));
                 }
                 for (var i = 0; i < 4; i++) {
-                    this.game.addEntity(new InfectedUnit(this.game, this.path2.startX, this.path2.startY + (i * 32 + 10), this.path2.path));
+                    this.game.addEntity(new InfectedHarpy(this.game, this.path2.startX, this.path2.startY + (i * 1.5), this.copyPath(this.path2.path)));
                 }
                 this.spawnThirdWave = true;
                 break;
@@ -95,33 +101,42 @@ class EnemySpawner {
             case this.timeElapsed > 40 && !this.spawnFourthWave:
                 //Path 1
                 for (var i = 0; i < 11; i++) {
-                    this.game.addEntity(new InfectedUnit(this.game, this.path1.startX, this.path1.startY + (i * -32), this.path1.path));
+                    this.game.addEntity(new InfectedUnit(this.game, this.path1.startX, this.path1.startY + (i * -1), this.copyPath(this.path1.path)));
                 }
                 for (var i = 0; i < 7; i++) {
-                    this.game.addEntity(new InfectedVenom(this.game, this.path1.startX, this.path1.startY + (i * -32 * 0.5), this.path1.path));
+                    this.game.addEntity(new InfectedVenom(this.game, this.path1.startX, this.path1.startY + (i * -2 - 3), this.copyPath(this.path1.path)));
                 }
                 for (var i = 0; i < 5; i++) {
-                    this.game.addEntity(new InfectedHarpy(this.game, this.path1.startX, this.path1.startY + (i * -32 - 10), this.path1.path));
+                    this.game.addEntity(new InfectedHarpy(this.game, this.path1.startX, this.path1.startY + (i * -2.5 - 0.5), this.copyPath(this.path1.path)));
                 }
-                for (var i = 0; i < 3; i++) {
-                    this.game.addEntity(new InfectedChubby(this.game, this.path2.startX, this.path2.startY + (i * 32 - 5), this.path2.path));
-                }
+                // for (var i = 0; i < 3; i++) {
+                //     this.game.addEntity(new InfectedChubby(this.game, this.path2.startX, this.path2.startY + (i), this.copyPath(this.path2.path)));
+                // }
 
                 //Path 2
                 for (var i = 0; i < 9; i++) {
-                    this.game.addEntity(new InfectedUnit(this.game, this.path2.startX, this.path2.startY + (i * 32), this.path2.path));
+                    this.game.addEntity(new InfectedUnit(this.game, this.path2.startX, this.path2.startY + (i * 1), this.copyPath(this.path2.path)));
                 }
                 for (var i = 0; i < 10; i++) {
-                    this.game.addEntity(new InfectedUnit(this.game, this.path2.startX, this.path2.startY + (i * 32 * 0.5), this.path2.path));
+                    this.game.addEntity(new InfectedVenom(this.game, this.path2.startX, this.path2.startY + (i * 2 + 1), this.copyPath(this.path2.path)));
                 }
                 for (var i = 0; i < 7; i++) {
-                    this.game.addEntity(new InfectedUnit(this.game, this.path2.startX, this.path2.startY + (i * 32 + 10), this.path2.path));
+                    this.game.addEntity(new InfectedHarpy(this.game, this.path2.startX, this.path2.startY + (i * 2.5 + 0.5), this.copyPath(this.path2.path)));
                 }
-                for (var i = 0; i < 2; i++) {
-                    this.game.addEntity(new InfectedChubby(this.game, this.path2.startX, this.path2.startY + (i * 32 + 5), this.path2.path));
-                }
+                // for (var i = 0; i < 2; i++) {
+                //     this.game.addEntity(new InfectedChubby(this.game, this.path2.startX, this.path2.startY + (i * 32 + 5), this.copyPath(this.path2.path)));
+                // }
                 this.spawnFourthWave = true;
                 break;    
         }
+    }
+
+    //Helper function to deep copy paths
+    copyPath(originalPath) {
+        let copiedPath = [];
+        for(let i = 0; i < originalPath.length; i++) {
+            copiedPath[i] = { x : originalPath[i].x, y : originalPath[i].y }
+        }
+        return copiedPath;
     }
 }
