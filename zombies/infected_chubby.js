95401fe53176fbaf0f8c4ff2fdeea7acb3bf1613
Manny Munoz <em66@uw.edu>
2021-03-08 20:53:45 -0800

    Fixed healthbar

--- zombies/infected_chubby.js
+++ zombies/infected_chubby.js
@@ -307,11 +307,11 @@ class InfectedChubby {
         var xOffset = 35;
         var yOffset = 35;
 
+        this.animations[this.state][this.facing].drawFrame(this.game.clockTick, ctx, this.x - xOffset - (this.game.camera.cameraX * PARAMS.BLOCKWIDTH), this.y - yOffset - (this.game.camera.cameraY * PARAMS.BLOCKWIDTH), 0.5);
+
         if (this.hitpoints < MAX_CHUBBY_HEALTH) {
             drawHealthbar(ctx, this.hitpoints, this.x, this.y, this.game, MAX_CHUBBY_HEALTH);
         }
-
-        this.animations[this.state][this.facing].drawFrame(this.game.clockTick, ctx, this.x - xOffset - (this.game.camera.cameraX * PARAMS.BLOCKWIDTH), this.y - yOffset - (this.game.camera.cameraY * PARAMS.BLOCKWIDTH), 0.5);
 
         if (PARAMS.DEBUG) {
             ctx.strokeStyle = "Red";
